ece6781d553b2fa37cb8e8e92eb95f1dbdc2fbf2
KareemD123 <kareemdraz.careers@gmail.com>
2024-04-04 12:57:47 -0400

    Form Date fix & mailto feature

--- Class-Notes/JS/Forms/script.js
+++ Class-Notes/JS/Forms/script.js
@@ -10,40 +10,42 @@ function handleFormSubmit() {
   const formData = formInputNames.map((inputName) => {
     const element = document.getElementById("event-form")[inputName];
     const inputValue = element.value;
-    // if (inputName == "eventName" && inputValue.length == 0) {
-    //   alert("Please type a name for your event!");
-    //   return null;
-    // }
-    // // For the details, we want a length of more than 50 characters
-    // if (inputName == "eventDetails" && inputValue.length < 10) {
-    //   alert("Please add more details to your event!");
-    //   return null;
-    // }
-    // // For the email, it should have an '@' symbol, and a .com or .org or .edu or .ca
-    // if (inputName == "eventEmail" && !inputValue.includes("@")) {
-    //   alert(
-    //     "Please include the @ symbol and a terminating address to your email"
-    //   );
-    //   return null;
-    // }
+    if (inputName == "eventName" && inputValue.length == 0) {
+      alert("Please type a name for your event!");
+      element.style.border = "2px solid red";
+      return null;
+    }
+    // For the details, we want a length of more than 50 characters
+    if (inputName == "eventDetails" && inputValue.length < 10) {
+      alert("Please add more details to your event!");
+      return null;
+    }
+    // For the email, it should have an '@' symbol, and a .com or .org or .edu or .ca
+    if (inputName == "eventEmail" && !inputValue.includes("@")) {
+      alert(
+        "Please include the @ symbol and a terminating address to your email"
+      );
+      return null;
+    }
     // For the event Date, it needs to be in the future!
     // TODO: Fix Bug
-    console.log(inputValue);
-    console.log(Date.now());
-    const now = new Date(Date.now());
-    const month = now.getMonth();
-    const date = now.getDate();
-    const inputMonth = inputValue.split("-")[1];
-    const inputDate = inputValue.split("-")[2];
-    if (inputName == "eventDate" && !inputMonth >= month) {
-      if (!inputDate >= date) {
-        alert("Event must be in the future!!!");
-      }
+    // console.log(Date.parse(inputValue)); // 2024-04-11
+    // console.log(Date.now()); // 171224790400
+
+    const inputDateInSeconds = Date.parse(inputValue);
+    const currentDateInSeconds = Date.now();
+    if (inputName == "eventDate" && inputDateInSeconds < currentDateInSeconds) {
+      alert("Event must be in the future!!!");
       return null;
     }
     return inputValue;
   });
-  console.log(formData);
+}
+
+function clearFormWarning(inputName) {
+  const inputElement = document.getElementById("event-form")[inputName];
+  inputElement.style.border = "unset";
+  // inputElement.style.border = "1px solid black";
 }
 
 // TODO: Figure out how to include .com/.org etc.
